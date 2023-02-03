9caff6ce8ce2122f861671a610fa7f5d2721fd38
Edward Hanson <ehanson787@gmail.com>
2023-02-03 15:19:35 +0000

    Tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
 
 # Create your tests here.
+class CustomUserTests(TestCase):
+    
+    def test_create_user(self):
+        User= get_user_model()
+        user= User.objects.create_user(
+            username='Eddy',
+            email='eddy@gmail.com',
+            age=12,
+            password='test123'
+            )
+        self.assertEqual(user.username,'Eddy')
+        self.assertEqual(user.email,'eddy@gmail.com')
+        self.assertEqual(user.age,12)
+        
+class SignupPageTests(TestCase):
+    
+    def test_signup_page_status_code(self):
+        response = self.client.get('/accounts/signup/')
+        self.assertEqual(response.status_code, 200)
+        
+    def test_view_url_by_name(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'registration/signup.html')
--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from .models import Todo
+from django.contrib.auth import get_user_model
 
 # Create your tests here.
+class TodoTests(SimpleTestCase):
+    
+    def setUp(self):
+        
+        user= get_user_model().objects.create_user(username='Eddy',email='eddy@gmail.com',age=12, password='test123')
+        todo= Todo.objects.create(task= 'Play with Django', author=self.user)
